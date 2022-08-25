3194e8e4193e8f51ee9ffa49be3705c7020329ba
Seiya830 <forest.sh90830@gmail.com>
2022-08-25 20:52:53 +0900

    clickイベント修正

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const btn = document.querySelector('.btn');
-const id = document.querySelector('.btn').getAttribute('href');
 
 btn.addEventListener('click', function() {
-  closest(document.querySelector('.btn'), 'div').style.display = 'none';
-  document.querySelector('id').classList.add('fit');
-  document.querySelector('id').style.display = 'block';
-});
+    const q_01 = document.querySelector('#q_01');
+    q_01.style.display = 'none';
+    document.querySelector('#q_01').getAttribute('href');
+    const q_02 = document.querySelector('#q_02');
+    q_02.classList.add('fit');
+    q_02.style.display = 'none';
+  });
 
 
 // $(function() {
