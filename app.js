0643f017e54a94015f4646cebcfbe0c00eb2886b
Seiya830 <forest.sh90830@gmail.com>
2022-08-28 22:31:12 +0900

    clickイベント試行錯誤中

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-document.querySelectorAll('.btn').forEach(function() {
+const btn = document.getElementsByClassName('btn');
 
-  this.addEventListener('click', function() {
-    const q_01 = document.querySelector('#q_01');
-    q_01.style.display = 'none';
-    document.querySelector('#q_01').getAttribute('href');
-    const q_02 = document.querySelector('#q_02');
-    q_02.style.display = 'block';
-  });
+btn.addEventListener('click', function() {
+  document.getElementById('q_02').style.display = 'none';
+
+    const q_02 = document.getElementsByClassName('q_02');
+    
+    if(q_02.style.display=='block') {
+      q_02.style.display = 'none';
+    }else {
+      q_02.style.display = 'block';
+    }
+  
 });
 
+
+
+// document.querySelectorAll('.btn1').forEach(function() {
+//   this.addEventListener('click', function() {
+//     const q_01 = document.querySelector('#q_01');
+//     q_01.style.display = 'none';
+//     document.querySelector('#q_01').getAttribute('href');
+//     const q_02 = document.querySelector('#q_02');
+//     q_02.style.display = 'block';
+//   });
+// });
 
 
 
